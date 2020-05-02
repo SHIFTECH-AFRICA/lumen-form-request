c7520d3ce4b3e0dc22dba5da5a80624c699a50c2
dev-techguy <dev.techguy@gmail.com>
2020-05-02 11:38:44 +0300

    :ok_hand: Updating code due to code review changes.

--- src/Commands/ModelMakeCommand.php
+++ src/Commands/ModelMakeCommand.php
@@ -3,20 +3,47 @@
 namespace ShiftechAfrica\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
 class ModelMakeCommand extends GeneratorCommand
 {
     /**
+     * The console command name.
+     *
      * @var string
      */
     protected $name = 'make:model';
 
     /**
+     * The console command description.
+     *
      * @var string
      */
-    protected $description = 'Make a new model.';
+    protected $description = 'Create a new Eloquent model class';
 
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
     protected $type = 'Model';
+
+    /**
+     * Execute the console command.
+     * @throws FileNotFoundException
+     */
+    public function handle()
+    {
+        if (parent::handle() !== false) {
+            if ($this->option('migration')) {
+                $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
+
+                $this->call('make:migration', ['name' => "create_{$table}_table", '--create' => $table]);
+            }
+        }
+    }
 
     /**
      * Get the stub file for the generator.
@@ -28,8 +55,27 @@ class ModelMakeCommand extends GeneratorCommand
         return __DIR__ . '/stubs/model.stub';
     }
 
-    protected function getDefaultNamespace($namespace)
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
     {
-        return $namespace;
+        return $rootNamespace;
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model.'],
+        ];
     }
 }
